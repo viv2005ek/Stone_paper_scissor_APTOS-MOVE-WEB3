2cd25e546246816333cddfcad36a2522a6216edf
vivek <viv2005ek@gmail.com>
2024-11-17 11:18:47 +0530

    final changes

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+
+const wallets = [new PetraWallet()];
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <AptosWalletAdapterProvider plugins={wallets} autoConnect={false}>
+      <App />
+    </AptosWalletAdapterProvider>
   </StrictMode>,
 )
